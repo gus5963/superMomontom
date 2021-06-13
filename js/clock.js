320788614b612dc16152ba2bf39d0f274bf93689
gus5963 <gus5963@gmail.com>
2021-06-13 19:06:31 +0900

    designing

--- js/clock.js
+++ js/clock.js
@@ -10,7 +10,8 @@ function getTime(){
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clock.innerHTML = `${years}. ${months+1}. ${dates} ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds <10 ? `0${seconds}` : seconds}`
+    clock.innerHTML = `${years}. ${months+1}. ${dates}
+    ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds <10 ? `0${seconds}` : seconds}`
 }
 
 
